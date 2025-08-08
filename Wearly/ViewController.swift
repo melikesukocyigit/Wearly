99061b5a6055665e30df98446ecf44c7d04875f4
melikesukocyigit <melikesukocyigt@hotmail.com>
2025-08-08 10:58:44 +0300

    Initial Commit

---
+++ Wearly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wearly
+//
+//  Created by Melike Su KOÇYİĞİT on 8.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
